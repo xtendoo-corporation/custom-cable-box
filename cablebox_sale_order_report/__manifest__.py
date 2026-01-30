8949e5919d33e28f0d8ffc30d9aee6494c5658a1
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-01-30 09:08:38 +0100

    format improved

--- cablebox_sale_order_report/__manifest__.py
+++ cablebox_sale_order_report/__manifest__.py
@@ -5,13 +5,13 @@
     "category": "Sale",
     "author": "Xtendoo",
     "license": "AGPL-3",
-    "depends": ["sale"],
+    "depends": ["sale", "sale_project"],
     "data": ["views/cablebox_report_sale_order.xml"],
-    'assets': {
-        'web.report_assets_pdf': [
-            'cablebox_sale_order_report/static/src/css/report_styles.css',
+    "assets": {
+        "web.report_assets_pdf": [
+            "cablebox_sale_order_report/static/src/css/report_styles.css",
         ],
     },
     "installable": True,
-    'application': True,
+    "application": True,
 }
