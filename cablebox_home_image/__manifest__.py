0ba14c880ddac9aba2003df0e955d2ffe0f1c9e5
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-02-06 10:52:03 +0100

    home image change

---
+++ cablebox_home_image/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Cablebox Home Menu Background",
+    "summary": "Añade una imagen de fondo personalizada al menú de aplicaciones",
+    "version": "18.0.1.0.0",
+    "category": "Web",
+    "author": "Guillermo Barcena Lopez",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "cablebox_home_image/static/src/scss/home_menu_background.scss",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
