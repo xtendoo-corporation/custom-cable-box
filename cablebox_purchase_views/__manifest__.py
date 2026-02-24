4ecad69a07369a37d96583ad8dd1cfabae21b075
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-02-24 09:41:00 +0100

    purchase views

---
+++ cablebox_purchase_views/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Cablebox Purchase Views",
+    "version": "18.0.1.0.0",
+    "category": "Purchases",
+    "summary": "Customizations for Purchase module views for Cablebox",
+    "author": "Cablebox",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
