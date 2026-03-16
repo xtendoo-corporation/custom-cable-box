e8bd769af1a0d449bdd6c0069662a36a4e3fa4e7
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-03-16 08:37:11 +0100

    column add

--- cablebox_purchase_views/models/stock_picking.py
+++ cablebox_purchase_views/models/stock_picking.py
@@ -5,19 +5,19 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     x_sale_origin = fields.Char(
-        related='sale_id.origin',
-        string='Documento origen',
+        related="sale_id.origin",
+        string="Documento origen",
         store=True,
         readonly=True,
     )
-# Copyright 2025 Cablebox
-from odoo import fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    custom_purchase_id = fields.Many2one(
-        comodel_name="purchase.order",
-        string="Documento de Compra",
+    purchase_origin = fields.Char(
+        string="Documento origen Compra",
+        related="purchase_id.origin",
+        store=False,
+        readonly=True,
     )
