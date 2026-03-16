77e9b0b6c055c4180d8f85f40140c69839067a9e
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-03-16 08:53:04 +0100

    relateds fixed

--- cablebox_purchase_views/models/stock_picking.py
+++ cablebox_purchase_views/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class StockPicking(models.Model):
@@ -15,9 +15,32 @@ class StockPicking(models.Model):
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    purchase_origin = fields.Char(
+    purchase_origin = fields.Many2one(
+        "purchase.order",
+        string="Pedido Compra",
+        compute="_compute_order_refs",
+        store=False,
+    )
+
+    sale_origin = fields.Many2one(
+        "sale.order",
+        string="Pedido Venta",
+        compute="_compute_order_refs",
+        store=False,
+    )
+    purchase_document_origin = fields.Char(
         string="Documento origen Compra",
         related="purchase_id.origin",
         store=False,
         readonly=True,
     )
+
+    @api.depends("picking_type_code", "sale_id", "purchase_id")
+    def _compute_order_refs(self):
+        for rec in self:  # ✅ ambas líneas dentro del for
+            rec.purchase_origin = (
+                rec.purchase_id if rec.picking_type_code == "incoming" else False
+            )
+            rec.sale_origin = (
+                rec.sale_id if rec.picking_type_code == "outgoing" else False
+            )
